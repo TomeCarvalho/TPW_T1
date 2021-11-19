4df32ef189febfc1edfdee3880b08c3085b9f66f
Tomé Carvalho <tomecarvalho@ua.pt>
2021-11-19 11:58:04 +0000

    Product page improvements, but image thumbnails not working except for the 1st one

--- djangoProject/app/admin.py
+++ djangoProject/app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from app.models import Group, Client, Product, Sale, ProductInstance, ProductImage
+from app.models import Group, Product, Sale, ProductInstance, ProductImage
 
 admin.site.register(Group)
-admin.site.register(Client)
 admin.site.register(Product)
 admin.site.register(Sale)
 admin.site.register(ProductInstance)
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -22,7 +22,7 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('products/<int:i>', views.product_page)
+    path('products/<int:i>', views.product_page),
     path('', views.dashboard),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
