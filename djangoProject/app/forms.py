64398e64a8b214fce3b95247ac1fb5cae0ddf38d
Tomé Carvalho <tomecarvalho@ua.pt>
2021-11-21 16:49:27 +0000

    Big description input area

--- djangoProject/app/forms.py
+++ djangoProject/app/forms.py
@@ -54,7 +54,7 @@ class ProductForm(forms.Form):
     name = forms.CharField(label="Name", max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
     stock = forms.IntegerField(label="Stock", widget=forms.NumberInput(attrs={'min': 0, 'max': 100, 'placeholder': 0}))
     description = forms.CharField(label="Description", max_length=1000,
-                                  widget=forms.TextInput(attrs={'placeholder': 'Description'}))
+                                  widget=forms.Textarea(attrs={'placeholder': 'Description', 'style': 'width: 50%'}))
     price = forms.FloatField(label="Price", widget=forms.NumberInput(attrs={'placeholder': 0}))
     group = forms.CharField(label="Group", max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Optional'}),
                             required=False)
